23e910b343e1c9e4b90955254e2b2fac2c7e0bc1
Daniel Pelajo <dpelajo@coppetec.ufrj.br>
2024-10-16 17:04:41 -0300

    configuração e commit inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,44 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    darkMode: ["class"],
+    content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+				common: {
+					branco: '#FFF',
+					preto: '#000'
+				},
+				'cinza-fumaca': {
+					main: '#666',
+					light: '#CCC',
+					dark: '#333',
+				},
+				primary: {
+					main: '#1976D2',
+					light: '#42A5F5',
+					dark: '#1565C0',
+				},
+				secondary: {
+					main: '#B0279B',
+					light: '#BA68C8',
+					dark: '#7B1FA2',
+				},
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [animate],
 };
 export default config;
