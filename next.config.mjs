e94704b90d09e71851fe4bc52c77f75cc91c2376
Daniel Pelajo <dpelajo@coppetec.ufrj.br>
2025-02-17 15:47:35 -0300

    Alteração nos remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   basePath: "/shortlink",
   images: {
     remotePatterns: [
-      { hostname: "localhost" }
+      { hostname: "localhost" },
+      { hostname: "192.168.0.146" },
+      { hostname: "bty.coppetec.org.br", protocol: "https" },
     ]
   }
 };
